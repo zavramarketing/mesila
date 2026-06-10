26688d73f8e57591f59d2e94a5d9ca2b2b21163b
zavramarketing <marketing@zavra.eu>
2026-06-10 23:17:49 +0300

    feat: wrap app with CartProvider and add checkout route

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import ProductDetail from './pages/ProductDetail';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="pood" element={<Shop />} />
-          <Route path="toode/:id" element={<ProductDetail />} />
-          <Route path="meist" element={<About />} />
-          <Route path="kontakt" element={<Contact />} />
-        </Route>
-      </Routes>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="pood" element={<Shop />} />
+            <Route path="toode/:id" element={<ProductDetail />} />
+            <Route path="meist" element={<About />} />
+            <Route path="kontakt" element={<Contact />} />
+            <Route path="tellimus" element={<Checkout />} />
+          </Route>
+        </Routes>
+      </CartProvider>
     </BrowserRouter>
   );
 }
